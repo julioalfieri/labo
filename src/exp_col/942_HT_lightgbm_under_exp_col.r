be492897d3f4679f5336bbb286d43f7ec9293db9
JulioAlfieri <alfieri.julio@gmail.com>
2022-10-26 01:12:48 +0000

    update

--- src/exp_col/942_HT_lightgbm_under_exp_col.r
+++ src/exp_col/942_HT_lightgbm_under_exp_col.r
@@ -21,9 +21,9 @@ require("mlrMBO")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "HT9420"
+PARAM$experimento <- "HT9421"
 
-PARAM$exp_input  <- "TS9320"
+PARAM$exp_input  <- "TS9321"
 # FIN Parametros del script
 
 
@@ -363,9 +363,9 @@ dtrain  <- lgb.Dataset( data=    data.matrix( dataset[ fold_train==1, campos_bue
                       )
 
 
-kvalidate  <- TRUE
-ktest  <- TRUE
-kcrossvalidation  <- FALSE
+kvalidate  <- FALSE
+ktest  <- FALSE
+kcrossvalidation  <- TRUE
 
 #Si hay que hacer validacion
 if( dataset[ fold_train==0 & fold_test==0 & fold_validate==1, .N ] > 0 )
